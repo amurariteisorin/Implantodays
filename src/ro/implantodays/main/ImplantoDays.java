8f70a4f55c26261c04cfdd30beef1bada96ee18d
vladbutnaru <vlad2me@gmail.com>
2016-09-18 15:14:04 +0300

    Added java files

---
+++ src/ro/implantodays/main/ImplantoDays.java
@@ -0,0 +1,9 @@
+package ro.implantodays.main;
+
+import java.util.logging.Logger;
+
+import ro.implantodays.utils.DBConnection;
+
+public class ImplantoDays {
+	Logger l = Logger.getLogger(ImplantoDays.class.getName());
+}
---
+++ src/ro/implantodays/utils/DBConnection.java
@@ -0,0 +1,53 @@
+package ro.implantodays.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.*;
+
+
+public class DBConnection {
+	static Connection conn = null;
+	Logger l = Logger.getLogger(DBConnection.class.getName());
+	public Connection getConnection(){
+		  try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			l.severe(e.getMessage());
+		}
+			try {
+				
+					
+			
+			  if(conn==null){
+				 
+		    conn =
+		        DriverManager.getConnection("jdbc:mysql://baniimei.online:3306/implantodays?" +
+		                                   "user=root&password=cocolino!");
+				
+		    l.info("Created new DB Connection");
+			  }
+			  if(conn.isClosed()){
+
+				  conn =
+						  DriverManager.getConnection("jdbc:mysql://baniimei.online:3306/implantodays?" +
+                                  "user=root&password=cocolino!");
+
+
+				  
+				  
+			  }
+		 
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		    l.severe("Error while creating Database Connection");
+		  
+		}
+		return conn;		
+	}
+}
